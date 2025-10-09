a84118ba8637c64f63abc61f5bdad2cb65155c64
darwin osingo <darwin.osingo@student.moringaschool.com>
2025-10-09 15:36:52 +0300

    practice

--- hello.c
+++ hello.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
- 
+#include <string.h>
+#include <ctype.h>
 
-int main(){
-    char name[50];
-    printf("whats your name \n");
-    fgets(name , sizeof(name), stdin);
+int main(void){
+
+    char vow[50];
+    printf("how many vowels in this?\n");
+    fgets(vow, sizeof(vow),stdin);
+
+    vow[strcspn(vow,"\n")]= '\0';
+
+    int count = 0;
+
+    
+
+   
+
+    printf("your fav word is %s\n",vow);
+    printf("it has %d vowels\n ", count);
+    return 0;
+
+
+
+
 
 }
