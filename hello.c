d75a29d365bc0844976f24071a1d1afd1f5d0cfb
darwin osingo <darwin.osingo@student.moringaschool.com>
2025-10-10 11:56:41 +0300

    letter counter practice

--- hello.c
+++ hello.c
@@ -2,23 +2,24 @@
 #include <string.h>
 #include <ctype.h>
 
-int main(void){
-
+int main (void){
     char vow[50];
-    printf("how many vowels in this?\n");
+    printf("whats your favorite word?\n");
     fgets(vow, sizeof(vow),stdin);
 
-    vow[strcspn(vow,"\n")]= '\0';
+    vow[strcmp(vow,"\n")]= '\0';
 
     int count = 0;
+    for( int i=0;vow[i] != '\0';i++){
+       if(isalpha((unsigned char)vow[i])){
+        count ++;
+        
+       }
 
-    
-
-   
-
+    }
     printf("your fav word is %s\n",vow);
-    printf("it has %d vowels\n ", count);
-    return 0;
+
+    printf("it has %d letters\n",count);
 
 
 
