ed3432ff102ce2e68d4e614d71f6f8e46bf6d0d9
darwin osingo <darwin.osingo@student.moringaschool.com>
2025-10-12 00:35:52 +0300

    perfected

--- hello.c
+++ hello.c
@@ -1,28 +1,41 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
+#include <math.h>
 
-int main (void){
-    char vow[50];
-    printf("whats your favorite word?\n");
-    fgets(vow, sizeof(vow),stdin);
+int main(void) {
+    double monthly, annual_rate;
+    int years;
+    char choice;
 
-    vow[strcmp(vow,"\n")]= '\0';
-
-    int count = 0;
-    for( int i=0;vow[i] != '\0';i++){
-       if(isalpha((unsigned char)vow[i])){
-        count ++;
+    do {
         
-       }
+        printf("\nEnter your monthly deposit amount (Ksh): ");
+        scanf("%lf", &monthly);
 
-    }
-    printf("your fav word is %s\n",vow);
+        printf("Enter annual interest rate (in %%): ");
+        scanf("%lf", &annual_rate);
 
-    printf("it has %d letters\n",count);
+        printf("Enter number of years: ");
+        scanf("%d", &years);
 
+        
+        double r = annual_rate / 100.0;   
+        double i = r / 12.0;              
+        int N = years * 12;               
 
+        double factor = pow(1.0 + i, N);
+        double A = monthly * (factor - 1.0) / i;
 
+        
+        printf("\nAfter %d years, you will have: Ksh %.2f\n", years, A);
+        printf("Your total contributions: Ksh %.2f\n", monthly * N);
+        printf("Interest earned: Ksh %.2f\n", A - (monthly * N));
 
+        
+        printf("\nDo you want to calculate again? (y/n): ");
+        scanf(" %c", &choice);  
 
+    } while (choice == 'y' || choice == 'Y');
+
+    printf("\nGoodbye.\n");
+    return 0;
 }
