dfd6a17cd9a7e0f20d95b1ab3b97ea2d04ee2412
darwin osingo <darwin.osingo@student.moringaschool.com>
2026-01-14 22:00:56 +0300

    under 30 seconds

--- smthg.c
+++ smthg.c
@@ -1,45 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+int main(){
+    char name[50];
+    float height;
+    int age;
+    char quote[150];
 
-int main(void){
-    char choice;
-    char vow[50];
-
-    do
-    {
-        printf("whats your fav word broski:\n");
-        fgets(vow,sizeof(vow),stdin);
-        
-
-        int count = 0;
-        for(int i=0;vow[i]!='\0';i++){
-        char c =tolower(vow[i]);
-        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
-            count++;
-        }
-        }
-
-        printf("your favorite word is %s\n",vow);
-        printf("it has %d letters \n",count);
-        printf("would you like to enter a diffrent word?(y/n)?\n");
-
-        scanf(" %c",&choice);
-        getchar();
-
-        
-
-
-
-
-    } while (choice=='y'||choice=='Y');
-
-    printf("\ngoodbye\n");
-    return 0;
-    
-
+    printf("Whats your favorite quote");
+    fgets(name,sizeof(name),stdin);
 
 }
-
-
-
